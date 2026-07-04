ec4631155c71fb032a6b7e755944bd40cb644a19
karamkaroeducation <karamkaroeducation@gmail.com>
2026-07-04 16:59:48 +0530

    Add image upload support for quotes

--- admin/quotes.js
+++ admin/quotes.js
@@ -1,11 +1,23 @@
-import { db } from "../firebase.js";
-import { collection, addDoc } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import { db, storage } from "../firebase.js";
 
+import {
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
 const publishBtn = document.getElementById("publishBtn");
 const quoteText = document.getElementById("quoteText");
+const quoteImage = document.getElementById("quoteImage");
 
 publishBtn.addEventListener("click", async () => {
   const quote = quoteText.value.trim();
+  const imageFile = quoteImage.files[0];
+let imageUrl = "";
 
   if (quote === "") {
     alert("Please write a quote.");
@@ -13,9 +25,15 @@ publishBtn.addEventListener("click", async () => {
   }
 
   try {
+    if (imageFile) {
+    const storageRef = ref(storage, "quotes/" + Date.now() + "_" + imageFile.name);
+    await uploadBytes(storageRef, imageFile);
+    imageUrl = await getDownloadURL(storageRef);
+    }
     await addDoc(collection(db, "quotes"), {
       text: quote,
-      createdAt: new Date()
+imageUrl: imageUrl,
+createdAt: new Date()
     });
 
     alert("Quote Published Successfully!");
